1c66a99ffecdbd821d0380201cc7e3816884c7e5
Daniel Justin <danieljmuleke@gmail.com>
2024-01-22 12:43:39 +0200

    Implemented login feature

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Router from './router'
+import elementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(elementPlus)
+  .use(Router)
+  .mount('#app')
